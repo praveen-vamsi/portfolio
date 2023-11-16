34803324a2bf7cd16f70b92cb9fd110a4fe9d284
praveen-vamsi <125984376+praveen-vamsi@users.noreply.github.com>
2023-11-16 16:23:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+    const menu = document.querySelector(".menulinks");
+    const icon = document.querySelector(".hamburg-icon");
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+}
